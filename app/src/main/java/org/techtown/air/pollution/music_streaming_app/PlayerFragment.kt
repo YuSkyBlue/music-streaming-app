129e4e6d409d9ac403dc7fe799030bd74e94558a
Chungkyu <dbcjdrb1997@gmail.com>
2021-12-31 15:03:53 +0900

    Initial commit

---
+++ app/src/main/java/org/techtown/air/pollution/music_streaming_app/PlayerFragment.kt
@@ -0,0 +1,17 @@
+package org.techtown.air.pollution.music_streaming_app
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+
+class PlayerFragment: Fragment(R.layout.fragment_player) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+    companion object{
+        fun newInstance(): PlayerFragment{
+            return  PlayerFragment()
+        }
+    }
+}
